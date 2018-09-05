d7ec1b57c054174596751301c8a49fbea6aa2a8a
bdjong <bobby.dejong1991@gmail.com>
2018-09-05 17:39:14 +0200

    json-simple parser Added

--- src/Scenes/SimulationScene.java
+++ src/Scenes/SimulationScene.java
@@ -2,7 +2,9 @@ package Scenes;
 
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
+import javafx.geometry.HPos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
@@ -39,15 +41,38 @@ public class SimulationScene {
 		bPane = new BorderPane();
 		//bPane.setStyle("-fx-background-color: #333333;");
 		bPane.setCenter(grid);
+		bPane.setMaxSize(800, 800);
 	}
 	
 	
 	public void setGrid(){
 		grid = new GridPane();
-		//grid.setGridLinesVisible(true);
-		grid.setMaxSize(300, 300);
-		grid.getColumnConstraints().add(new ColumnConstraints(32));        
-		grid.getRowConstraints().add(new RowConstraints(32));
+		grid.setGridLinesVisible(true);
+		grid.setMaxSize(800, 800);
+		
+		int cols = 10;
+		int rows = 10;
+						
+		for (int i = 0; i < cols; i++) {
+		      ColumnConstraints colConst = new ColumnConstraints();
+		      colConst.setPercentWidth(100.0 / cols);
+		      grid.getColumnConstraints().add(colConst);
+		}
+		for (int i = 0; i < rows; i++) {
+		    RowConstraints rowConst = new RowConstraints();
+		    rowConst.setPercentHeight(100.0 / rows);
+		    grid.getRowConstraints().add(rowConst);         
+		} 		
+		
+		
+		for (int i = 0; i<10; i++)
+		{
+			for (int j = 0; j < 10; j++)
+			{
+				grid.add(new Label(i+","+j), i, j);
+			}
+		}		
+		
 		//grid.setStyle("-fx-background-image: url('/img/ground.png')");
 	}
 	
@@ -66,7 +91,7 @@ public class SimulationScene {
 	 * method die een scene opbouwd voor het spel
 	 */	
 	public void setSimulationScene(){
-		simulationScene = new Scene(bPane, 646, 706);
+		simulationScene = new Scene(bPane, 1000, 1000);
 		simulationStage.setScene(simulationScene);
 	}
 
